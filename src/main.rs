a915d1ca8fdac8f74385a684d535c9d910392d6d
lequeux <maxence.lequeux.etu@univ-lille.fr>
2021-10-27 11:44:51 +0200

    simple implementation of cd

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
 use std::io::{Write};
 
-fn main() {
-    let mut input = String::new();
+fn process_command(cmd_string: &String) -> bool {
     let mut splitted: std::str::SplitWhitespace;
-    let mut count: usize;
+    let count: usize;
+    let first_word: &str;
+    let will_continue = true;
 
-    loop {
-        print!(">");
-        std::io::stdout().flush().unwrap();
-        input.clear();
-        std::io::stdin().read_line(&mut input).unwrap();
-        splitted = input.split_whitespace();
-        count = splitted.clone().count();
+    splitted = cmd_string.split_whitespace();
+    count = splitted.clone().count();
 
-        if count == 0 {
-            eprintln!("Could not parse command");
+    if count == 0 {
+        eprintln!("Could not parse command");
+        return will_continue;
+    }
+
+    first_word = splitted.next().unwrap();
+
+    match first_word {
+        "cd" => {
+            match splitted.next() {
+                Some(path) => {
+                    match std::env::set_current_dir(path) {
+                        Ok(_) => (),
+                        Err(_) => eprintln!("Invalid path")
+                    }
+                },
+                None => eprintln!("No path provided")
+            }
         }
-        else {
-            let mut command = std::process::Command::new(splitted.next().unwrap());
+        _ => {
+            let mut command = std::process::Command::new(first_word);
 
             for arg in splitted {
                 command.arg(arg);
             }
-
+        
             match command.spawn() {
                 Ok(mut child) => {
                     child.wait().unwrap();
@@ -31,6 +43,23 @@ fn main() {
                     eprintln!("Invalid command");
                 }
             }
+        }
+    }
+
+    return will_continue;
+}
+
+fn main() {
+    let mut input = String::new();
+
+    loop {
+        print!("{}|>", std::env::current_dir().unwrap().display());
+        std::io::stdout().flush().unwrap();
+        input.clear();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        if ! process_command(&input) {
+            break;
         }
     }
 }
